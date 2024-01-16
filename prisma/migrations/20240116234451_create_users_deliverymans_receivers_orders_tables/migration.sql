2de19456dc561c4c186d50b9ddfce338886041e0
Danrlei <danrleidalfre@gmail.com>
2024-01-16 20:48:33 -0300

    create schema prisma and config postgres docker

---
+++ prisma/migrations/20240116234451_create_users_deliverymans_receivers_orders_tables/migration.sql
@@ -0,0 +1,66 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('DELIVERYMAN', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'DELIVERYMAN',
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "deliverymans" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "deliverymans_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "receivers" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+
+    CONSTRAINT "receivers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "orders" (
+    "id" TEXT NOT NULL,
+    "deliveryman_id" TEXT NOT NULL,
+    "receiver_id" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "available_at" TIMESTAMP(3),
+    "withdrawal_at" TIMESTAMP(3),
+    "delivered_at" TIMESTAMP(3),
+    "returned_at" TIMESTAMP(3),
+    "photo_url" TEXT,
+
+    CONSTRAINT "orders_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_cpf_key" ON "users"("cpf");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "deliverymans_email_key" ON "deliverymans"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "receivers_email_key" ON "receivers"("email");
+
+-- AddForeignKey
+ALTER TABLE "deliverymans" ADD CONSTRAINT "deliverymans_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "orders" ADD CONSTRAINT "orders_deliveryman_id_fkey" FOREIGN KEY ("deliveryman_id") REFERENCES "deliverymans"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "orders" ADD CONSTRAINT "orders_receiver_id_fkey" FOREIGN KEY ("receiver_id") REFERENCES "receivers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
